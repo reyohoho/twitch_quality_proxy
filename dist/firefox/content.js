f4d44648bf34ee2afeebac9acceec992d509ca04
reyohoho <reyohoho@proton.me>
2026-06-18 10:14:08 +0300

    2.6.0

--- dist/firefox/content.js
+++ dist/firefox/content.js
@@ -182,7 +182,7 @@
 // ReYohoho Twitch Proxy - Constants
 // ============================================
 
-const VERSION = '2.5.2';
+const VERSION = '2.6.0';
 const PROXY_SERVERS = [
     "https://proxy4.rte.net.ru/",
     "https://proxy7.rte.net.ru/",
--- dist/userscript/reyohoho-twitch.user.js
+++ dist/userscript/reyohoho-twitch.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ReYohoho Twitch Proxy + VAFT
 // @namespace    https://github.com/reyohoho
-// @version      2.5.2
+// @version      2.6.0
 // @description  Прокси для Twitch с поддержкой 1080p/1440p; опция «Скрыть Audio Only» в настройках плеера
 // @author       ReYohoho
 // @match        https://www.twitch.tv/*
@@ -911,7 +911,7 @@
 // ReYohoho Twitch Proxy - Constants
 // ============================================
 
-const VERSION = '2.5.2';
+const VERSION = '2.6.0';
 const PROXY_SERVERS = [
     "https://proxy4.rte.net.ru/",
     "https://proxy7.rte.net.ru/",
